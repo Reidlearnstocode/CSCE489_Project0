f2a36b43e2ce744c710f69cae6e73996f5bc6150
Reidlearnstocode <169181658+Reidlearnstocode@users.noreply.github.com>
2024-06-29 10:48:23 -0400

    Update hello.c

--- c_proj0/hello.c
+++ c_proj0/hello.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 int main(){
-printf("Hello, world!\n");
+printf("Holy cow, this is working!\n");
+  printf("I'm not quite sure that I did everything right though, and as of 29 Jun 24, I am having some difficulty integrating git to the virtual machine.\n");
+  printf("Last but not least, I haven't figured out how to transfer using WinSCP as the suggested protocol was not available,\n");
 return 0;
 }
